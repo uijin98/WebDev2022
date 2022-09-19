81f6e47037441097b5a4bd32a06f2696575b75ff
uijin98 <story0856@naver.com>
2022-09-19 16:33:26 +0900

    파이썬날짜 실습

--- test.py
+++ test.py
@@ -1,16 +1,10 @@
-ouput_a="{:d}".format(52)
+import datetime
 
-output_b="{:5d}".format(52)
-output_c="{:10d}".format(52)
+now=datetime.datetime.now()
 
-output_d="{:5d}".format(52)
-output_e="{:10d}".format(-52)
-
-print('# 기본')
-print(output_a)
-print("#특정 칸에 출력하기")
-print(output_b)
-print(output_c)
-print("#빈칸을 0으로 채우기")
-print(output_d)
-print(output_e)
+print(now.year,"년")
+print(now.month,"월")
+print(now.day, "일")
+print(now.hour,"시")
+print(now.minute,"분")
+print(now.second,"초")
