2dc4cb9bdb256a0aed6e26b06f45682862750642
uijin98 <story0856@naver.com>
2022-10-27 22:53:16 +0900

    소분설

---
+++ CUpSampleDlg.cpp
@@ -0,0 +1,48 @@
+﻿// CUpSampleDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ImageProcessing_2020108251.h"
+#include "CUpSampleDlg.h"
+#include "afxdialogex.h"
+
+
+// CUpSampleDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CUpSampleDlg, CDialogEx)
+
+CUpSampleDlg::CUpSampleDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG5, pParent)
+	, m_UpSampleRate(0)
+{
+
+}
+
+CUpSampleDlg::~CUpSampleDlg()
+{
+}
+
+void CUpSampleDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_UpSampleRate);
+}
+
+
+BEGIN_MESSAGE_MAP(CUpSampleDlg, CDialogEx)
+	ON_EN_CHANGE(IDC_EDIT1, &CUpSampleDlg::OnEnChangeEdit1)
+END_MESSAGE_MAP()
+
+
+// CUpSampleDlg 메시지 처리기
+
+
+void CUpSampleDlg::OnEnChangeEdit1()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CDialogEx::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
